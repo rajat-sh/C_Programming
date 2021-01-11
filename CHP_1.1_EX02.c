9a225dff4692c493b3c118c229413966f212db6a
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2021-01-11 18:17:27 +0100

    Add files via upload

---
+++ CHP_1.1_EX02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/* A Program to print Celsius to Fahrenheit table of Celsius temperature in range from 0, 20, 40, ........, 300 
+   with floating-point arithmetic */
+
+
+main()
+
+
+{	
+	/* Declare the variables with their data types */
+	float fahr, celsius;
+	int lower, upper, step;
+
+	/* Initialize the value of the variables */
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	printf("\nFahrenheit to Celsius table\n\n");
+	
+	fahr = lower;
+
+	while ( fahr <= upper )
+	{
+		celsius = 5.0/9.0 * ( fahr - 32.0 );
+		printf("%3.0f\t%6.1f\n", fahr , celsius );
+		fahr = fahr + step;
+	}
+
+}
+
+
+
+	
