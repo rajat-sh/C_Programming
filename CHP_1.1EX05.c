522cb34d52baa539f1fd19884175c4176fab0e0d
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2021-01-12 18:49:19 +0100

    Add files via upload

---
+++ CHP_1.1EX05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* A program to print fahr-celsius table using for loop */
+
+main()
+
+
+{
+
+
+	int fahr;
+	
+	for ( fahr = 0; fahr <= 300; fahr = fahr +20 )
+		printf("%3d\t%1.2f\n", fahr, (5.0/9.0) * ( fahr -32 ) );
+
+}
