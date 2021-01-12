f107ececcecfbbd8ecc1867912954cc71f517d0d
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2021-01-12 18:51:44 +0100

    Update CHP_1.1_EX05.c

--- CHP_1.1_EX05.c
+++ CHP_1.1_EX05.c
@@ -10,7 +10,7 @@ main()
 
 	int fahr;
 	
-	for ( fahr = 0; fahr <= 300; fahr = fahr +20 )
+	for ( fahr = 0; fahr <= 300; fahr = fahr + 20 )
 		printf("%3d\t%1.2f\n", fahr, (5.0/9.0) * ( fahr -32 ) );
 
 }
