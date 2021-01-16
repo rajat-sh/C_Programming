e6979b099cb95f1e0d4d6d31df08f78c1fb9caef
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2021-01-16 15:20:20 +0100

    Add files via upload

---
+++ CHP_1.1_EX08.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/* Copy input to output */
+
+main ()
+
+
+
+{
+
+
+
+	int c;
+	
+	c = getchar ();
+
+	while ( c != EOF )
+	{	
+		putchar (c);
+		c = getchar ();
+
+	}
+
+
+}
