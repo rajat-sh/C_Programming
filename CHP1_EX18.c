050cef582cd56443e23da45021ab1c830b273a33
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2021-01-21 12:43:18 +0100

    Add files via upload

---
+++ CHP1_EX18.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define  IN 1  /* sate to represent in word */
+#define OUT 0  /* state to represent out of word */
+
+/* A program to count the number of characters, newlines and words */
+
+
+
+main()
+
+
+{
+
+
+	int c, nc, wc, nl, state;
+	
+	nc = nl = wc = 0;
+
+	state = OUT;
+
+	while ( ( c = getchar() )  != EOF )
+	{
+		++nc;
+		if ( c == '\n' )
+			++nl;
+		if ( c == ' ' || c == '\n' || c == '\t' )
+			state = OUT;
+		else if ( state == OUT )
+		{
+			++wc;
+			state = IN;
+		}
+
+	}
+	
+	printf ( "Character Count: %d\nWord Count:%d\nNew line Count:%d\n" , nc, wc, nl );
+
+
+	
+
+
+}
+
