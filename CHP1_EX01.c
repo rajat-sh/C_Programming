448f7848f0fee08299fccca060ee7f83ae6771f9
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2021-02-16 16:13:15 +0100

    Update CHP1_EX01.c

--- CHP1_EX01.c
+++ CHP1_EX01.c
@@ -19,8 +19,7 @@ main()
 	
 	fahr = lower;
 	
-	while ( fahr <= upper ) 
-	{
+	while ( fahr <= upper ) {
 		celsius = 5 * ( fahr - 32 ) /9;
 		printf("%3d\t%6d\n", fahr, celsius );
 		fahr = fahr + step;
